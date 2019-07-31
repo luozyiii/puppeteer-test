7d8da8fe21b4372864868bbe0c79c60a399d2136
罗智毅 <luozhiyi@123>
2019-07-31 15:58:57 +0800

    node支持import 语法支持

--- src/demo04.js
+++ src/demo04.js
@@ -1,10 +1,32 @@
 /*
   import 语法支持
+
+  1.全局安装
+  npm install babel-cli -g
+
+  2.本地安装
+  npm install babel-cli --save  or yarn add babel-cli
+
+  3.安装babel-preset-es2015
+  yarn add babel-preset-es2015
+
+  4. 添加.babelrc 文件
+  {
+    "presets": [
+      "es2015"
+    ],
+    "plugins": []
+  }
+
+  5.运行
+  全局babel-cli: babel-node src/demo04.js
+  本地babel-cli：在本地package.json 的scripts 添加命令 "babel": "babel-node src/demo04.js"; 然后执行npm run babel
+
 */
 
 import {
   EXECUTABLEPATH_WIN,
-} from '@/config'
+} from './config'
 
 const puppeteer = require('puppeteer')
 
@@ -15,7 +37,7 @@ const options = {
   executablePath: pathToExtension // 运行绑定的 Chromium 版本
 }
 
-const A = async () => {
+const D = async () => {
   const browser = await puppeteer.launch(options)
   const page = await browser.newPage()
   await page.goto('https://www.baidu.com')
@@ -24,4 +46,4 @@ const A = async () => {
   await browser.close();
 }
 
-A()
+D()
